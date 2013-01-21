8f87cdabd2f2f3ca4fcf8c6082a204958ba78c02
unknown <Administrator@P1150-Dfische2.bpc.mpg.de>
2013-01-21 11:29:48 +0100

    COM7 zu COM 8 geändert

--- combivac.cpp
+++ combivac.cpp
@@ -10,7 +10,7 @@ combivac::combivac(QObject *parent) :
                           FLOW_OFF,
                           -1),parent)
 {
-    setPortName("COM7") ;
+    setPortName("COM8") ;
     open(ReadWrite) ;
 }
 
